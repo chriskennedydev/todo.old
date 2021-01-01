94911737d075f69f2edef4cced1e515d058c7146
Christopher Kennedy <csk86.05.14@gmail.com>
2021-01-01 06:55:11 -0600

    Memory Leaaaakgit status!

--- src/main.c
+++ src/main.c
@@ -75,6 +75,7 @@ int main(int argc, char *argv[])
 
                 check_dir(homedir, workdir);
                 add(item, filedir);
+                free(item);
         }
 
         if(check_delete == 0)
